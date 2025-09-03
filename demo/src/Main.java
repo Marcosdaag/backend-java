b79a10ce67857406cf8130ac62475e252a68fff2
Marcosdaag <marcosoffs99@gmail.com>
2025-09-03 20:47:55 -0300

    links y clase 2

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -3,17 +3,8 @@
 public class Main {
 
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
-
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-      System.out.println("i = " + i);
-      System.out.println("i = " + i);
-      System.out.println("i = " + i);
+      String message = "Hola😎 \"Entre comillas\" ";
+      System.out.println(message); //Agrega salto de linea
+      System.out.print(message); //Sin salto de linea
     }
   }
-}
