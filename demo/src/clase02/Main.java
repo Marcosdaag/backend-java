5f0af53cbdb3b92d0d38f453cafc242858d11435
Marcosdaag <marcosoffs99@gmail.com>
2025-09-10 19:58:19 -0300

    condicionales y bucles

---
+++ demo/src/clase02/Main.java
@@ -0,0 +1,13 @@
+package clase02;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String ingresoUsuario = scanner.nextLine();
+
+    System.out.println(ingresoUsuario);
+  }
+}
